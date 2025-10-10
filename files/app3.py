78a414d618512af1db19c60eee5fb2d9b8d4f44b
NeerajBIG <neeraj.kumar@bitsinglass.com>
2025-10-10 01:25:58 -0400

    req file

--- files/app3.py
+++ files/app3.py
@@ -513,7 +513,7 @@ def main():
     }
     </style>""", unsafe_allow_html=True)
 
-    st.text("test "+controller.get('cookie_name'))
+    st.text("testing "+ str(controller.get('cookie_name')))
     if controller.get('cookie_name') == 'Guest':
         sidebar_navigation()
     elif controller.get('cookie_name') == 'None':
