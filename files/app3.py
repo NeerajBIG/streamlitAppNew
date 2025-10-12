9abdd2e2f99992abb0c236355c72fd2136941ec4
NeerajBIG <neeraj.kumar@bitsinglass.com>
2025-10-11 22:53:30 -0400

    req file

--- files/app3.py
+++ files/app3.py
@@ -192,6 +192,8 @@ def login():
                 check_params = (result[0]['id'],)
                 record_exists = db.fetch_data(check_query, check_params)
                 st.text(record_exists)
+                if record_exists == "[{'COUNT(*)': 0}]":
+                    st.text("record_existsffffffffffffffffff")
 
                 if record_exists[0] > 0:
                     st.text("Record FOUNDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
